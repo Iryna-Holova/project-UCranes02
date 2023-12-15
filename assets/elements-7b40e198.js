5b00a409822b885734f14cb4baf0784b8db91c02
Iryna-Holova <94083644+Iryna-Holova@users.noreply.github.com>
2023-12-15 04:58:43 +0000

    Deploying to gh-pages from @ Iryna-Holova/project-UCranes02@81d50d8cbab884d986eca27e3f49205a0cdf4ea7 🚀

--- assets/elements-7b40e198.js
+++ assets/elements-7b40e198.js
@@ -1,4 +1,4 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&i(s)}).observe(document,{childList:!0,subtree:!0});function c(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=c(e);fetch(e.href,t)}})();function l(o,r){return o.map(({_id:c,bodyPart:i,name:e,target:t,rating:s,burnedCalories:n,time:a})=>`
+(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&i(r)}).observe(document,{childList:!0,subtree:!0});function c(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=c(e);fetch(e.href,t)}})();function a(o,s){return o.map(({_id:c,bodyPart:i,name:e,target:t,rating:r,burnedCalories:n,time:l})=>`
          <li class="main-item_card-exercises">
       <a class="link-exercise-card" href="" data-id="${c}">
         <!-- Top place card -->
@@ -7,7 +7,7 @@
             <div class="const-text-exer">
               <p class="text-card-exer">WORKOUT</p>
             </div>
-            ${r==="favorites"?`
+            ${s==="favorites"?`
                 <button class="btn-delete-card" type="button">
                 <svg class="icon-delete-favorite" width="16" height="16">
                   <use href="/img/icons.svg#icon-remove"></use>
@@ -16,7 +16,7 @@
                 
               `:`
                 <div class="cont-card-rating">
-                  <p class="card-rating-exer">${s}</p>
+                  <p class="card-rating-exer">${r}</p>
                   <svg class="icon-card-exer" width="18" height="18">
                     <use href="img/icons.svg#icon-star"></use>
                   </svg>
@@ -40,7 +40,7 @@
           <ul class="list-info-exer">
             <li class="item-card-exer">
               <p class="text-title-item-exer big">Burned calories:</p>
-              <p class="text-info-exer size-time">${n}/${a}</p>
+              <p class="text-info-exer size-time">${n}/${l}</p>
             </li>
             <li class="item-card-exer">
               <p class="text-title-item-exer medium">Body part:</p>
@@ -53,5 +53,5 @@
           </ul>
         </div>
       </a>
-    </li>`).join("")}const d={burger:document.querySelector(".js-burger"),filter:document.querySelector(".js-filter"),gallery:document.querySelector(".js-gallery"),nav_home:document.querySelector(".nav-home"),nav_favorites:document.querySelector(".nav-favorites"),pagination:document.querySelector(".js-pagination"),subscribe_form:document.querySelector(".js-subscribe-form"),quote:document.querySelector(".js-quote")};export{l as a,d as e};
-//# sourceMappingURL=elements-b7c45304.js.map
+    </li>`).join("")}const d={burger:document.querySelector(".js-burger"),filter:document.querySelector(".js-filter"),gallery:document.querySelector(".js-gallery"),pagination:document.querySelector(".js-pagination"),searchForm:document.querySelector(".js-search-form"),subscribe_form:document.querySelector(".js-subscribe-form"),quote:document.querySelector(".js-quote")};export{a,d as e};
+//# sourceMappingURL=elements-7b40e198.js.map
