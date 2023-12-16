02f8eeeddd89f6ce06f0b08656558dea2e1ebae0
Iryna-Holova <94083644+Iryna-Holova@users.noreply.github.com>
2023-12-16 15:04:30 +0000

    Deploying to gh-pages from @ Iryna-Holova/project-UCranes02@25bfbab329b9b5ea647dd230b19e0948ae5d6cd5 🚀

--- assets/elements-c26e840c.js
+++ assets/elements-c26e840c.js
@@ -54,4 +54,4 @@
         </div>
       </a>
     </li>`).join("")}const f={burger:document.querySelector(".js-burger"),filter:document.querySelector(".js-filter"),gallery:document.querySelector(".js-gallery"),gallerySubtitle:document.querySelector(".js-subtitle"),pagination:document.querySelector(".js-pagination"),searchForm:document.querySelector(".js-search-form"),backBtn:document.querySelector(".js-back-button"),subscribeForm:document.querySelector(".js-subscribe-form"),quote:document.querySelector(".js-quote"),policyBtn:document.querySelector(".js-policy-modal-open"),termsBtn:document.querySelector(".js-terms-modal-open"),scrollTopBtn:document.querySelector(".js-scroll-top-button")};export{m as a,f as e,l as r};
-//# sourceMappingURL=elements-3ec99d5b.js.map
+//# sourceMappingURL=elements-c26e840c.js.map
