3d3788ed7d315b3f514e79cd6281c1ea8290f264
Iryna-Holova <94083644+Iryna-Holova@users.noreply.github.com>
2023-12-16 10:28:51 +0000

    Deploying to gh-pages from @ Iryna-Holova/project-UCranes02@55997c663c0afd4c272aac91652ae98a7a860d8a 🚀

--- assets/elements-b7a7bf32.js
+++ assets/elements-b7a7bf32.js
@@ -54,4 +54,4 @@
         </div>
       </a>
     </li>`).join("")}const f={burger:document.querySelector(".js-burger"),filter:document.querySelector(".js-filter"),gallery:document.querySelector(".js-gallery"),gallerySubtitle:document.querySelector(".js-subtitle"),pagination:document.querySelector(".js-pagination"),searchForm:document.querySelector(".js-search-form"),backBtn:document.querySelector(".js-back-button"),subscribeForm:document.querySelector(".js-subscribe-form"),quote:document.querySelector(".js-quote"),policyBtn:document.querySelector(".js-policy-modal-open"),termsBtn:document.querySelector(".js-terms-modal-open")};export{m as a,f as e,l as r};
-//# sourceMappingURL=elements-5f1f1464.js.map
+//# sourceMappingURL=elements-b7a7bf32.js.map
--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import{e as a,a as r}from"./assets/elements-5f1f1464.js";const e=JSON.parse(localStorage.getItem("favorites"));console.log(e);a.gallery.insertAdjacentHTML("beforeend",r(e,"favorites"));
+import{e as a,a as r}from"./assets/elements-b7a7bf32.js";const e=JSON.parse(localStorage.getItem("favorites"));console.log(e);a.gallery.insertAdjacentHTML("beforeend",r(e,"favorites"));
 //# sourceMappingURL=commonHelpers.js.map
