d9ef3820de69e4ac36349df7e84467471d3a4a67
Iryna-Holova <94083644+Iryna-Holova@users.noreply.github.com>
2023-12-16 19:44:42 +0000

    Deploying to gh-pages from @ Iryna-Holova/project-UCranes02@cc3dc1375f74cab56a5ec76739f8822ec171a148 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import{e as a,a as r}from"./assets/elements-56abefd3.js";const e=JSON.parse(localStorage.getItem("favorites"));console.log(e);a.gallery.insertAdjacentHTML("beforeend",r(e,"favorites"));
+import{e as a,a as r}from"./assets/elements-6d2bf3b7.js";const e=JSON.parse(localStorage.getItem("favorites"));console.log(e);a.gallery.insertAdjacentHTML("beforeend",r(e,"favorites"));
 //# sourceMappingURL=commonHelpers.js.map
