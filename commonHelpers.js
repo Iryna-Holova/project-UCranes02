3bc12d86772b26d9e60c31f4a74fb8c0255e5d27
Iryna-Holova <94083644+Iryna-Holova@users.noreply.github.com>
2023-12-18 13:39:22 +0000

    Deploying to gh-pages from @ Iryna-Holova/project-UCranes02@3f33402c8a643fcb22f176ad1e3c905599b222dd 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import{e,h as l,a as n,f as r,b as t}from"./assets/favorites-page-6becd58d.js";import{l as o}from"./assets/vendor-243d56f5.js";window.addEventListener("scroll",o(t,500));e.btnOpenBurger.addEventListener("click",l);e.btnCloseBurger.addEventListener("click",l);e.scrollTopBtn.addEventListener("click",n);r.load();
+import{e,h as l,a as n,f as r,b as t}from"./assets/favorites-page-397f73cf.js";import{l as o}from"./assets/vendor-243d56f5.js";window.addEventListener("scroll",o(t,500));e.btnOpenBurger.addEventListener("click",l);e.btnCloseBurger.addEventListener("click",l);e.scrollTopBtn.addEventListener("click",n);r.load();
 //# sourceMappingURL=commonHelpers.js.map
