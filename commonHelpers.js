2b728454fbdcded3f9b9f35458dac62c85af66e1
Iryna-Holova <94083644+Iryna-Holova@users.noreply.github.com>
2023-12-18 05:24:38 +0000

    Deploying to gh-pages from @ Iryna-Holova/project-UCranes02@a9503925fb77ffdfbc46df762ae8d014cce0f527 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import{e,h as l,a as n,f as r,b as t}from"./assets/favorites-page-97f10668.js";import{l as o}from"./assets/vendor-243d56f5.js";window.addEventListener("scroll",o(t,500));e.btnOpenBurger.addEventListener("click",l);e.btnCloseBurger.addEventListener("click",l);e.scrollTopBtn.addEventListener("click",n);r.load();
+import{e,h as l,a as n,f as r,b as t}from"./assets/favorites-page-e06db4d7.js";import{l as o}from"./assets/vendor-243d56f5.js";window.addEventListener("scroll",o(t,500));e.btnOpenBurger.addEventListener("click",l);e.btnCloseBurger.addEventListener("click",l);e.scrollTopBtn.addEventListener("click",n);r.load();
 //# sourceMappingURL=commonHelpers.js.map
--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import{e,a as l,h as r,g as n,r as t,b as o}from"./assets/favorites-page-97f10668.js";import{l as s}from"./assets/vendor-243d56f5.js";window.addEventListener("scroll",s(o,100));e.scrollTopBtn.addEventListener("click",l);e.btnOpenBurger.addEventListener("click",r);e.btnCloseBurger.addEventListener("click",r);n.load();t.observe(document.querySelector("html"));
+import{e,a as l,h as r,g as n,r as t,b as o}from"./assets/favorites-page-e06db4d7.js";import{l as s}from"./assets/vendor-243d56f5.js";window.addEventListener("scroll",s(o,100));e.scrollTopBtn.addEventListener("click",l);e.btnOpenBurger.addEventListener("click",r);e.btnCloseBurger.addEventListener("click",r);n.load();t.observe(document.querySelector("html"));
 //# sourceMappingURL=commonHelpers2.js.map
